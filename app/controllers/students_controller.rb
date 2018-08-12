cb1511e29868f121304455ba8836bb4acb10eb06
derrick b <dbozich1@gmail.com>
2018-08-12 19:47:34 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,9 +14,9 @@ class StudentsController < ApplicationController
   def create
     # @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
     # @student = Student.find(params[:id])
-    session[:form_params] = params.inspect
-    
-    binding.pry
+    # session[:form_params] = params.inspect
+    # 
+    # binding.pry
     redirect_to student_path(Student.find_by(first_name: params['first_name']))
   end
 
